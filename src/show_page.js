20bb0c5543460e06a752ac619a94b7f432881d96
Samuel Song <samsongs@umich.edu>
2021-12-27 19:31:37 -0600

    Setup testing for chart js

--- src/show_page.js
+++ src/show_page.js
@@ -17,6 +17,60 @@ import { loadSizePage } from './size_page'
 // To render index page
 import { loadIndexPage } from './index_page'
 
+// Chart.js
+import {
+    Chart,
+    ArcElement,
+    LineElement,
+    BarElement,
+    PointElement,
+    BarController,
+    BubbleController,
+    DoughnutController,
+    LineController,
+    PieController,
+    PolarAreaController,
+    RadarController,
+    ScatterController,
+    CategoryScale,
+    LinearScale,
+    LogarithmicScale,
+    RadialLinearScale,
+    TimeScale,
+    TimeSeriesScale,
+    Decimation,
+    Filler,
+    Legend,
+    Title,
+    Tooltip
+  } from 'chart.js';
+  
+  Chart.register(
+    ArcElement,
+    LineElement,
+    BarElement,
+    PointElement,
+    BarController,
+    BubbleController,
+    DoughnutController,
+    LineController,
+    PieController,
+    PolarAreaController,
+    RadarController,
+    ScatterController,
+    CategoryScale,
+    LinearScale,
+    LogarithmicScale,
+    RadialLinearScale,
+    TimeScale,
+    TimeSeriesScale,
+    Decimation,
+    Filler,
+    Legend,
+    Title,
+    Tooltip
+  );
+
 // ====================================================
 // =============== C O N S T A N T S ==================
 // ====================================================
@@ -102,7 +156,6 @@ function loadShowPageStructure(selected_pokemon) {
     
     index_page_button.addEventListener("click", () => loadIndexPage(selected_pokemon));
     size_page_button.addEventListener("click", () => loadSizePage(selected_pokemon));
-
 }
 
 // Fetch data for selected pokemon and cache
@@ -275,6 +328,50 @@ function loadStats(current_pokemon) {
     stats_info.appendChild(special_attack);
     stats_info.appendChild(special_defense);
     stats_info.appendChild(speed);
+
+    // ==========================================================
+    // testing chart js
+    const ctx = document.createElement("canvas");
+    ctx.setAttribute("id", "chart");
+    ctx.setAttribute("width", 100);
+    ctx.setAttribute("height", 100);
+    stats.appendChild(ctx);
+    const myChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+            datasets: [{
+                label: '# of Votes',
+                data: [12, 19, 3, 5, 2, 3],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
+            }
+        }
+    });
+    console.log(myChart);
+    // ==========================================================
 }
 
 // Setup html elements for damage_multiplier
