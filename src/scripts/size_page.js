225dad9aa76482ff5c6e29af7869e1bdcd336f52
Samuel Song <samsongs@umich.edu>
2022-03-02 21:50:02 -0700

    Edit css for size page

--- src/scripts/size_page.js
+++ src/scripts/size_page.js
@@ -6,13 +6,7 @@
 import { capitalize, convertHeight } from './helpers'
 
 // Cache of pokemon data
-import { POKEMON_NAMES, TYPES, SELECTED_POKEMON } from './store.js'
-
-// To render show page
-import { loadShowPage } from './show_page'
-
-// To render index page
-import { loadIndexPage } from './index_page'
+import { POKEMON_NAMES, SELECTED_POKEMON } from './store.js'
 
 // ====================================================
 // =============== C O N S T A N T S ==================
@@ -26,10 +20,10 @@ const IMAGE_HTS = { trainer: 400 };
 // ================== E X P O R T S ===================
 // ====================================================
 
-export function loadSizePage(selected_pokemon) {
-    loadSizePageStructure(selected_pokemon);
+export function loadSizePage() {
+    loadSizePageStructure();
     loadTrainer();
-    loadSprites(selected_pokemon);
+    loadSprites();
 }
 
 // ====================================================
@@ -38,15 +32,14 @@ export function loadSizePage(selected_pokemon) {
 // ====================================================
 
 // Setup html elements for size page
-function loadSizePageStructure(selected_pokemon) {
+function loadSizePageStructure() {
     const main = document.querySelector("main");
     main.setAttribute("id", "size_page");
     main.innerHTML = "";
 
     const size_container = document.createElement("section");
+    const slider_container = document.createElement("div");
     const slider = document.createElement("input");
-    const show_page_button = document.createElement("button");
-    const index_page_button = document.createElement("button");
 
     size_container.setAttribute("id", "size_container");
     slider.setAttribute("id", "slider");
@@ -55,17 +48,11 @@ function loadSizePageStructure(selected_pokemon) {
     slider.setAttribute("max", 150);
     slider.setAttribute("value", 100);
 
-    show_page_button.innerHTML = "Go to show page";
-    index_page_button.innerHTML = "Go to index page";
-
+    main.appendChild(slider_container);
     main.appendChild(size_container);
-    main.appendChild(slider);
-    main.appendChild(show_page_button);
-    main.appendChild(index_page_button);
+    slider_container.appendChild(slider);
 
     slider.addEventListener("input", e => magnify(e.target.value));
-    show_page_button.addEventListener("click", () => loadShowPage(selected_pokemon));
-    index_page_button.addEventListener("click", () => loadIndexPage(selected_pokemon));
 }
 
 // Load trainer
@@ -92,17 +79,17 @@ function loadTrainer() {
 }
 
 // Load sprites
-function loadSprites(selected_pokemon) {
+function loadSprites() {
     const size_container = document.getElementById("size_container");
 
-    for(let id in selected_pokemon.selection) {
+    for(let id in SELECTED_POKEMON.selection) {
         let tile = document.createElement("div");
         let sprite_img = document.createElement("img");
         let name = document.createElement("p");
         let info = document.createElement("p");
 
-        let pokemon = POKEMON[id];
-        let img_url = pokemon.sprites.front_default;
+        let pokemon = POKEMON_NAMES[id];
+        let img_url = pokemon.sprite_url;
         const ft = convertHeight(pokemon.height);
         let img_ht = (ft * 650) / 5.5;
 
