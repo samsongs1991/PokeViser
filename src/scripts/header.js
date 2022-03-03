8673365be7c26dc60c0305ca9e5a0831a17613eb
Samuel Song <samsongs@umich.edu>
2022-03-02 18:01:14 -0700

    Refactor header

--- src/scripts/header.js
+++ src/scripts/header.js
@@ -13,35 +13,31 @@ export function loadHeader() {
     // Create html elements
     const body = document.querySelector("body");
     const header = document.createElement("header");
-    const title_container = document.createElement("div");
-    const title = document.createElement("img");
+    const logo_container = document.createElement("div");
+    const logo = document.createElement("img");
     const navbar = document.createElement("nav");
     const navlist = document.createElement("ul");
-    const home = document.createElement("li");
     const search = document.createElement("li");
     const about = document.createElement("li");
     
     // Set up html structure
     body.appendChild(header);
-    header.appendChild(title_container);
+    header.appendChild(logo_container);
     header.appendChild(navbar);
-    title_container.appendChild(title);
+    logo_container.appendChild(logo);
     navbar.appendChild(navlist);
-    navlist.appendChild(home);
     navlist.appendChild(search);
     navlist.appendChild(about);
 
-    // Set title attributes to display logo
-    title.setAttribute("src", "resources/pokeviser_img.png");
-    title.setAttribute("id", "logo");
+    // Set logo attributes to display logo
+    logo.setAttribute("src", "resources/logo.png");
+    logo.setAttribute("id", "logo");
     
     // Set ability for navbar elements to be clickable
-    title.addEventListener("click", goToSearchPage);
-    home.addEventListener("click", loadTitlePage);
+    logo.addEventListener("click", loadTitlePage);
     search.addEventListener("click", goToSearchPage);
         
     // Set text of navbar elements
-    home.innerHTML = "Home";
-    search.innerHTML = "Poke Search";
+    search.innerHTML = "Search";
     about.innerHTML = "About";
 }
